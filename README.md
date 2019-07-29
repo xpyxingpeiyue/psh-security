2d3aa73254d83e626357a3a662538825640d8886
xingpeiyue <xingpeiyue@163.com>
2019-07-29 16:46:28 +0800

    项目初始化

---
+++ README.md
@@ -0,0 +1,11 @@
+#### Spring-Security权限管理
+## 
+#### 模块介绍
+1. psh-security-core  
+核心代码库，依赖所有第三方库
+2. psh-security-app  
+处理app安全
+3. psh-security-browser  
+处理浏览器安全 
+4. psh-security-demo  
+程序入口，调用browser和 app  
---
+++ psh-security-demo/src/main/java/com/psh/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.psh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by peiyue.xing on 2019/7/29 14:20
+ * @author peiyue.xing
+ */
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class);
+    }
+}
