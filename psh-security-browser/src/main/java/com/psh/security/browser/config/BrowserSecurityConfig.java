ba4a3d2ccab86989dd0a7570aeb2ad019dab73fc
xingpeiyue <xingpeiyue@163.com>
2019-08-12 18:34:40 +0800

    添加layui模板页面

--- psh-security-browser/src/main/java/com/psh/security/browser/config/BrowserSecurityConfig.java
+++ psh-security-browser/src/main/java/com/psh/security/browser/config/BrowserSecurityConfig.java
@@ -12,18 +12,19 @@ import org.springframework.security.web.access.ExceptionTranslationFilter;
  * Created by peiyue.xing on 2019/8/3 12:35
  * security 配置
  * 安全：认证（authentication ）  授权 （authorization）
- *
+ * <p>
  * 原理
  * 请求->(UsernamePasswordAuthenticationFilter)(表单请求方式filter)->BasicAuthenticationFilter(默认请求filter)、、、->ExceptionTranslationFilter(用来捕获后面一个过滤器抛出的异常)->FilterSecurityInterceptor(Spring Security 过滤器的最后一环，所有前面都会经过这一环)
- *
+ * <p>
  * Spring Security 过滤器链->TESTful
+ *
  * @author peiyue.xing
  */
 @Configuration
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
     //定义加密解密方式
     @Bean
-    public  PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();//这是springsecurity定义的PasswordEncoder ，如果想用自己的方式秩序实现PasswordEncoder 就行
     }
 
@@ -32,9 +33,13 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
 //            http.httpBasic();//默认弹出窗模式
         http.formLogin()//规定表单登录方式
+                .loginPage("/page/login/login.html")//指定登录页
                 .and()
                 .authorizeRequests()//然后授权请求方式
+                .antMatchers("/page/login/login.html","/**/*.css","/**/*.js","/favicon.ico").permitAll()//指定不拦截
                 .anyRequest()//任何请求
-                .authenticated();//都需要身份认证
+                .authenticated()//都需要身份认证
+                .and()
+                .csrf().disable();//关闭防御攻击
     }
 }
